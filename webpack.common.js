32efc8d8ae30dac4ef5b13cbb0d31b14d9bd9497
Victor Mukumbu | Software Developer <57131903+VictorMukumbu@users.noreply.github.com>
2026-06-08 10:46:59 +0300

    dev and prod configs set up

--- webpack.common.js
+++ webpack.common.js
@@ -1,14 +1,19 @@
 // webpack.config.js
 import path from "node:path";
+import { fileURLToPath } from "node:url";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 
+const __filename =fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename);
+
 export default {
-  mode: "development",
-  entry: "./src/index.js",
+  entry: {
+    app:"./src/index.js",
+  },
   output: {
-    filename: "main.js",
-    path: path.resolve(import.meta.dirname, "dist"),
-    clean: true,
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, 'dist'),
+     clean: true,
   },
   //webpack-dev-server
   devtool: "eval-source-map",
